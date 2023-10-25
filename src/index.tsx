57a92fc04d44ba9c97539779f4b73a2f7f8afd52
Young Eric <erikbonopart12@gmail.com>
2023-10-25 12:59:12 +0300

    change BrowserRouter to Hash for github pages work

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
-import React, {StrictMode} from 'react';
+import React from 'react';
 import './index.css';
 
 
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 
 import {Provider} from "react-redux";
 import {store} from "./state/store";
 import {App} from "./app/App";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const container  = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
 root.render(
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
 );
 
 
